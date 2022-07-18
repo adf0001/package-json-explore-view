c010cd0a8654595f291c05c6ecc69e9a0484e99f
fwg <adf0001@163.com>
2022-07-18 12:31:34 +0800

    update: new package-json-treeview module; add package.json detail;

--- package-json-explore-view.js
+++ package-json-explore-view.js
@@ -7,6 +7,7 @@ var bind_ui = require("bind-ui");
 
 var package_json_to_html = require("package-json-to-html");
 var package_json_treeview = require("package-json-treeview");
+var json_container_treeview = require("json-container-treeview");
 
 require("htm-tool-css");	//require ht css
 
@@ -15,11 +16,15 @@ var packageJsonExploreView = {
 		htmlText: require("./package-json-explore-view.htm"),
 
 		init: "init",
+
+		bindArray: [
+			"showDetail", ["evt", "click", "onClickShowDetail"],
+		],
 	},
 
 	packageDataset: null,		//a package_json_data_set object, refer package-json-data-set @ npm
-
-	treeview: null,
+	
+	curPkg:null,	//current pkg
 
 	init: function (el) {
 		var _this = this;
@@ -33,11 +38,15 @@ var packageJsonExploreView = {
 			[this.nme('info'),], null, 50
 		);
 
-		this.treeview = new package_json_treeview.class(this.nme('.list'));
-		this.treeview.nameClickCallback = function (err, data) {
-			if (err) return;
-			_this.updateInfo(data);
-		}
+		package_json_treeview.initView(
+			this.nme('.list'), null,
+			{
+				nameClickCallback: function (err, data) {
+					if (err) return;
+					_this.updateInfo(data);
+				}
+			}
+		);
 	},
 
 	localLabel: null,		// refer package_json_to_html @npm
@@ -59,13 +68,22 @@ var packageJsonExploreView = {
 			versionStyle: isDirect ? "" : "color:red;",
 		};
 
-		this.nme('info').innerHTML = "<div style='overflow:auto;height:100%;white-space:nowrap;'>" +
+		this.nme('info.summary').innerHTML = "<div style='white-space:nowrap;'>" +
 			package_json_to_html(pkgItem.pkg, opt) + "</div>";
+
+		var elDetail = this.nme('info.detail');
+		elDetail.innerHTML = "";
+		this.curPkg= pkgItem.pkg;
+	},
+
+	onClickShowDetail: function (evt) {
+		var elDetail = this.nme('info.detail');
+		json_container_treeview.initView(elDetail, this.curPkg);
 	},
 
 	updateView: function (packageDataset) {
 		this.packageDataset = packageDataset;
-		this.treeview.updateView(packageDataset);
+		package_json_treeview.updateView(this.nme('.list'), packageDataset);
 
 		//info
 		this.updateInfo(this.packageDataset.top);
